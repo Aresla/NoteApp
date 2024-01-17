95a639bf39b2d5216276cf31e74906eff9144d47
Aresla <areslas@list.ru>
2024-01-17 17:32:45 +0200

    add frontend part

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,222 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
-function App() {
+interface Note {
+  id: number;
+  title: string;
+  content: string;
+}
+
+
+const App = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState(""); 
+  const [selectedNote, setSelectedNote] = useState<Note | null>(null);
+
+  // const [notes, setNotes] = useState([
+  //   {
+  //     id: 1,
+  //     title: "test note 1",
+  //     content: "bla bla note1",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "test note 2 ",
+  //     content: "bla bla note2",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "test note 3",
+  //     content: "bla bla note3",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "test note 4 ",
+  //     content: "bla bla note4",
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "test note 5",
+  //     content: "bla bla note5",
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "test note 6",
+  //     content: "bla bla note6",
+  //   },
+  // ]);
+
+  const [notes, setNotes] = useState<Note[]>([]);
+
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:5000/api/notes"
+        );
+
+        const notes: Note[] =
+          await response.json();
+
+        setNotes(notes);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchNotes();
+  }, []);
+  
+  // const handleAddNote = (event: React.FormEvent) => {
+  //   event.preventDefault();
+  //   console.log("title: ", title);
+  //   console.log("content: ", content);
+
+  //   const newNote: Note = {
+  //     id: notes.length + 1,
+  //     title: title,
+  //     content: content,
+  //   };
+
+  //   setNotes([newNote, ...notes]);
+  //   setTitle("");
+  //   setContent("");
+  // };
+
+  const handleAddNote = async (
+    event: React.FormEvent
+  ) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        "http://localhost:5000/api/notes",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title,
+            content,
+          }),
+        }
+      );
+
+      const newNote = await response.json();
+
+      setNotes([newNote, ...notes]);
+      setTitle("");
+      setContent("");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleNoteClick = (note: Note) => {
+    setSelectedNote(note);
+    setTitle(note.title);
+    setContent(note.content);
+  };
+
+  const handleUpdateNote = (event: React.FormEvent) => {
+    event.preventDefault();
+  
+    if (!selectedNote) {
+      return;
+    }
+  
+    const updatedNote: Note = {
+      id: selectedNote.id,
+      title: title,
+      content: content,
+    };
+  
+    const updatedNotesList = notes.map((note) => (note.id === selectedNote.id ? updatedNote : note));
+  
+    setNotes(updatedNotesList);
+    setTitle("");
+    setContent("");
+    setSelectedNote(null);
+  };
+
+  const handleCancel = () => {
+    setTitle("");
+    setContent("");
+    setSelectedNote(null);
+  };
+
+  // const deleteNote = (event: React.MouseEvent, noteId: number) => {
+  //   event.stopPropagation();
+  
+  //   const updatedNotes = notes.filter((note) => note.id !== noteId);
+  
+  //   setNotes(updatedNotes);
+  // };
+
+  const deleteNote = async (
+    event: React.MouseEvent,
+    noteId: number
+  ) => {
+    event.stopPropagation();
+
+    try {
+      await fetch(
+        `http://localhost:5000/api/notes/${noteId}`,
+        {
+          method: "DELETE",
+        }
+      );
+      const updatedNotes = notes.filter(
+        (note) => note.id !== noteId
+      );
+
+      setNotes(updatedNotes);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <form className="note-form" onSubmit={(event) => (selectedNote ? handleUpdateNote(event) : handleAddNote(event))} >
+        <input
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          placeholder="Title"
+          required>
+        </input>
+        <textarea
+          value={content}
+          onChange={(event) => setContent(event.target.value)}
+          placeholder="Content"
+          rows={10}
+          required>            
+        </textarea>
+        {selectedNote ? (
+          <div className="edit-buttons">
+            <button type="submit">Save</button>
+            <button onClick={handleCancel}>Cancel</button>
+          </div>
+          ) : (
+            <button type="submit">Add Note</button>
+        )}
+      </form>
+      <div className="notes-grid">
+        {notes.map((note) => (
+          <div className="note-item" onClick={() => handleNoteClick(note)}>
+            <div className="notes-header">
+            <button onClick={(event) => deleteNote(event, note.id)}>x</button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
+            
+          </div>
+  ))}
+</div>
     </div>
   );
-}
+};
 
 export default App;
